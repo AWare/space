1070363da2e01c4611076be47c447cbe496455e5
Alex Ware <alex.ware@guardian.co.uk>
2018-11-19 17:18:30 +0000

    no console.

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,6 @@ export class App extends React.Component {
         const coords = translate(thing.s,[canvas.width/2,canvas.height/2,0],0.1)
         const h = scale(thing.m)
         const v = Math.min(95,Math.max(0,(((thing.s[2] / canvas.width) + 1) * (95/2)))) + 5
-        console.log(v)
         ctx.fillStyle = `hsl(${h} 100% ${v}%)`;
         ctx.beginPath();
         ctx.ellipse(
